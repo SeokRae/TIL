e6bc0fcf36c10118dbd71338a51a76740d9c1bee
seokrae <kslbsh@gmail.com>
2021-10-07 21:29:56 +0900

    feat: init security once jwt

---
+++ security-once-jwt/src/main/java/com/example/securityoncejwt/SecurityOnceJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityoncejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOnceJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOnceJwtApplication.class, args);
+    }
+
+}
---
+++ security-once-jwt/src/test/java/com/example/securityoncejwt/SecurityOnceJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityoncejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOnceJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
