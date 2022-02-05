88f3dc6e9f292e1823c9d5e1c73b6416b147eda3
seokrae <kslbsh@gmail.com>
2022-02-05 16:31:03 +0900

    feat: enum을 통해 db의 코드성 데이터 코드로 관리

---
+++ java-enum/src/main/java/com/example/enumcode/JavaEnumApplication.java
@@ -0,0 +1,13 @@
+package com.example.enumcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEnumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEnumApplication.class, args);
+    }
+
+}
